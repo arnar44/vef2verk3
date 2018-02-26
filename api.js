7224ece31188ec883a479155a60262e964ea0bb2
arnar44 <arnar44@gmail.com>
2018-02-26 09:33:36 +0000

    first commit

--- api.js
+++ api.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const xss = require('xss');
+const { body, validationResult } = require('express-validator/check');
+const { sanitize } = require('express-validator/filter');
 
 const {
   create,
@@ -10,10 +13,113 @@ const {
 
 const router = express.Router();
 
+const jsonValidation = [
+  sanitize(['title', 'text', 'datetime']).trim(),
+  body('title').isLength({ min: 1, max: 255 }).withMessage({
+    field: 'title', message: 'Title must be a string of length 1 to 255 characters',
+  }),
+  body('text').custom(value => typeof value === 'string').withMessage({
+    field: 'text', message: 'Text must be a string',
+  }),
+  body('datetime').isISO8601().withMessage({
+    field: 'datetime', message: 'Datetime must be ISO 8601 date',
+  }),
+];
+
+async function data(req, res) {
+  const rows = await readAll();
+  return res.json(rows);
+}
+
+async function idData(req, res) {
+  const { id } = req.params;
+  const row = await readOne(id);
+
+  if (row) {
+    return res.json(row);
+  }
+
+  return res.status(404).json({ error: 'Not found' });
+}
+
+async function createNote(req, res) {
+  // fá öll gögn úr innsenda json
+  const {
+    body: {
+      title = '',
+      text = '',
+      datetime = '',
+    } = {},
+  } = req;
+
+    // öll gögn hreinsuð
+  const readyData = {
+    title: xss(title),
+    text: xss(text),
+    datetime: xss(datetime),
+  };
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const errorMessages = errors.array().map(i => i.msg);
+    return res.status(400).json(errorMessages);
+  }
+
+  const inserted = await create(readyData);
+  return res.status(201).json(inserted);
+}
+
+async function updateNote(req, res) {
+  const { id } = req.params;
+  // fá öll gögn úr innsenda json
+  const {
+    body: {
+      title = '',
+      text = '',
+      datetime = '',
+    } = {},
+  } = req;
+
+  // öll gögn hreinsuð
+  const readyData = {
+    title: xss(title),
+    text: xss(text),
+    datetime: xss(datetime),
+  };
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    const errorMessages = errors.array().map(i => i.msg);
+    return res.status(400).json(errorMessages);
+  }
+
+  const updated = await update(id, readyData);
+  return res.status(201).json(updated);
+}
+
+async function idDelete(req, res) {
+  const { id } = req.params;
+  const existed = await del(id);
+
+  // id var til, náðum að eyða
+  if (existed) {
+    return res.status(204).end();
+  }
+
+  return res.status(404).json({ error: 'Not found' });
+}
+
 function catchErrors(fn) {
   return (req, res, next) => fn(req, res, next).catch(next);
 }
 
-/* todo útfæra api */
+router.get('/', catchErrors(data));
+router.get('/:id', catchErrors(idData));
+router.post('/', jsonValidation, catchErrors(createNote));
+router.delete('/:id', catchErrors(idDelete));
+router.put('/:id', jsonValidation, catchErrors(updateNote));
+
 
 module.exports = router;
--- notes.js
+++ notes.js
@@ -1,6 +1,7 @@
 /* todo sækja pakka sem vantar  */
+const { Client } = require('pg');
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URL || 'postgres://Arnar:12345@localhost/v3';
 
 /**
  * Create a note asynchronously.
@@ -13,7 +14,22 @@ const connectionString = process.env.DATABASE_URL;
  * @returns {Promise} Promise representing the object result of creating the note
  */
 async function create({ title, text, datetime } = {}) {
-  /* todo útfæra */
+  const client = new Client({ connectionString });
+  await client.connect();
+
+  const query = 'INSERT INTO notes(datetime, title, text) VALUES ($1, $2, $3) RETURNING *';
+  const values = [datetime, title, text];
+
+  try {
+    const result = await client.query(query, values);
+    const { rows } = result;
+    return rows;
+  } catch (err) {
+    console.error('Error inserting data');
+    throw err;
+  } finally {
+    await client.end();
+  }
 }
 
 /**
@@ -22,7 +38,19 @@ async function create({ title, text, datetime } = {}) {
  * @returns {Promise} Promise representing an array of all note objects
  */
 async function readAll() {
-  /* todo útfæra */
+  const client = new Client({ connectionString });
+  await client.connect();
+
+  try {
+    const result = await client.query('SELECT * FROM notes');
+    const { rows } = result;
+    return rows;
+  } catch (err) {
+    console.error('Error selecting from data');
+    throw err;
+  } finally {
+    await client.end();
+  }
 }
 
 /**
@@ -33,7 +61,19 @@ async function readAll() {
  * @returns {Promise} Promise representing the note object or null if not found
  */
 async function readOne(id) {
-  /* todo útfæra */
+  const client = new Client({ connectionString });
+  await client.connect();
+
+  try {
+    const result = await client.query('SELECT * FROM notes WHERE id=$1', [id]);
+    const { rows } = result;
+    return rows[0];
+  } catch (err) {
+    console.error('Error selecting from data');
+    throw err;
+  } finally {
+    await client.end();
+  }
 }
 
 /**
@@ -48,7 +88,22 @@ async function readOne(id) {
  * @returns {Promise} Promise representing the object result of creating the note
  */
 async function update(id, { title, text, datetime } = {}) {
-  /* todo útfæra */
+  const client = new Client({ connectionString });
+  await client.connect();
+
+  const query = 'UPDATE notes SET datetime = $1, title = $2, text = $3 WHERE id=$4 RETURNING *';
+  const values = [datetime, title, text, id];
+
+  try {
+    const result = await client.query(query, values);
+    const { rows } = result;
+    return rows;
+  } catch (err) {
+    console.error('Error updating data');
+    throw err;
+  } finally {
+    await client.end();
+  }
 }
 
 /**
@@ -59,7 +114,21 @@ async function update(id, { title, text, datetime } = {}) {
  * @returns {Promise} Promise representing the boolean result of creating the note
  */
 async function del(id) {
-  /* todo útfæra */
+  const client = new Client({ connectionString });
+  await client.connect();
+
+  try {
+    // skilar id ef það var til (hægt að eyða) annars []
+    const deleted = await client.query('DELETE FROM notes WHERE id=$1 RETURNING id', [id]);
+    const { rowCount } = deleted;
+    // Skilar 0 ef id ekki til, annars 1
+    return rowCount;
+  } catch (err) {
+    console.error('Error deleting from data');
+    throw err;
+  } finally {
+    await client.end();
+  }
 }
 
 module.exports = {
